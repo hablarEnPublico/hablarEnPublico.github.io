06ef2cae1e7384b817003c1f1bda937b063b52f0
hablarEnPublico <shampoosnaturalesmasterclass@gmail.com>
2023-03-27 16:44:59 -0400

    Version 3

--- codigo.js
+++ codigo.js
@@ -1,11 +1,3 @@
 
 "use strict";
 
-
-// Codigo seccion For You - Img
-const foryou_img = document.querySelector(".foryou-img");
-
-const foryou_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting){ foryou_img.classList.add("foryou_img_view");  }   });  }
-
-const foryou_img_watch = new IntersectionObserver(foryou_img_view);
-foryou_img_watch.observe(foryou_img);
