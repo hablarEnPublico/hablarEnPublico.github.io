8af956745e45975ef377a04087d2d26ac01b6c44
hablarEnPublico <shampoosnaturalesmasterclass@gmail.com>
2023-03-31 13:09:46 -0400

    Commit 11

--- codigo.js
+++ codigo.js
@@ -10,3 +10,15 @@ const problema_view =(entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isInters
 
 const problema_watch = new IntersectionObserver(problema_view);
 problema_watch.observe(problema_container);
+
+
+
+//  Codigo efecto para problema2-container 
+
+const problema2_container = document.querySelector(".problema2-container");
+
+const problema2_view =(entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting){ problema2_container.classList.add("problema2-container-view"); }
+                                                              else problema2_container.classList.remove("problema2-container-view");   }); }
+
+const problema2_watch = new IntersectionObserver(problema2_view);
+problema2_watch.observe(problema2_container);
