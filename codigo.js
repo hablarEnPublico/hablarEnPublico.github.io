0ef7eb22c2f099c14ed2de0907f24106d79fca7d
hablarEnPublico <shampoosnaturalesmasterclass@gmail.com>
2023-04-04 00:06:23 -0400

    Commit edit

--- codigo.js
+++ codigo.js
@@ -26,3 +26,35 @@ problema2_watch.observe(problema2_container);
 
 
 
+// Codigo seccion curso
+
+const curso_img = document.querySelector(".curso-img");
+const curso_content = document.querySelector(".curso-content");
+
+let mq = matchMedia("(max-width:720px)");
+
+if(mq.matches) { const curso_img = document.querySelector(".curso-img");
+                 const curso_content = document.querySelector(".curso-content");
+                 console.log(mq.matches);
+                 curso_img.style.transform = 'translateX(0)';
+                 curso_content.style.transform = 'translateX(0)';
+
+	             const curso_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting) { curso_img.classList.add("curso-img-view-phone");
+                                                                                            curso_content.classList.add("curso-content-view-phone");  }
+                                                               else { curso_img.classList.remove("curso-img-view-phone");
+                                                                      curso_content.classList.remove("curso-content-view-phone"); }  });  }
+
+                 const curso_img_watch = new IntersectionObserver(curso_img_view);
+                 curso_img_watch.observe(curso_img); }
+
+else { const curso_img = document.querySelector(".curso-img");
+       const curso_content = document.querySelector(".curso-content");
+       console.log(mq.matches);
+
+	   const curso_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting) { curso_img.classList.add("curso-img-view");
+                                                                                            curso_content.classList.add("curso-content-view");  }
+                                                               else { curso_img.classList.remove("curso-img-view");
+                                                                      curso_content.classList.remove("curso-content-view"); }  });  }
+
+       const curso_img_watch = new IntersectionObserver(curso_img_view);
+       curso_img_watch.observe(curso_img); }
