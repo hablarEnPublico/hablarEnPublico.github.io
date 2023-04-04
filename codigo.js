af2f3481b938245a462cf103f53a8e068a66b4b2
hablarEnPublico <shampoosnaturalesmasterclass@gmail.com>
2023-04-04 01:00:57 -0400

    Commit better

--- codigo.js
+++ codigo.js
@@ -26,35 +26,45 @@ problema2_watch.observe(problema2_container);
 
 
 
+
+
+
+
+
+
+
+
 // Codigo seccion curso
 
 const curso_img = document.querySelector(".curso-img");
 const curso_content = document.querySelector(".curso-content");
-
 let mq = matchMedia("(max-width:720px)");
 
-if(mq.matches) { const curso_img = document.querySelector(".curso-img");
-                 const curso_content = document.querySelector(".curso-content");
-                 console.log(mq.matches);
-                 curso_img.style.transform = 'translateX(0)';
-                 curso_content.style.transform = 'translateX(0)';
+const inf_720px = ()=>{ const curso_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting) { curso_img.style.transform = `translateY(0)`;
+                                                                                                                    curso_content.style.transform = `translateY(0)`;
+                                                                                                                    curso_content.style.opacity = "1";  }
 
-	             const curso_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting) { curso_img.classList.add("curso-img-view-phone");
-                                                                                            curso_content.classList.add("curso-content-view-phone");  }
-                                                               else { curso_img.classList.remove("curso-img-view-phone");
-                                                                      curso_content.classList.remove("curso-content-view-phone"); }  });  }
+                                                                                       else { curso_img.style.transform = `translateY(25%)`;
+                                                                                              curso_content.style.transform = `translateY(-25%)`;
+                                                                                              curso_content.style.opacity = "0"; }  });  }
 
-                 const curso_img_watch = new IntersectionObserver(curso_img_view);
-                 curso_img_watch.observe(curso_img); }
+                        const curso_img_watch = new IntersectionObserver(curso_img_view);
+                        curso_img_watch.observe(curso_img); }
 
-else { const curso_img = document.querySelector(".curso-img");
-       const curso_content = document.querySelector(".curso-content");
-       console.log(mq.matches);
+// funcion para superior a 720px
 
-	   const curso_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting) { curso_img.classList.add("curso-img-view");
-                                                                                            curso_content.classList.add("curso-content-view");  }
+const sup_720px = ()=>{ const curso_img_view = (entryes)=>{ entryes.forEach(entrada=>{ if(entrada.isIntersecting) { curso_img.classList.add("curso-img-view");
+                                                                                                                    curso_content.classList.add("curso-content-view");  }
                                                                else { curso_img.classList.remove("curso-img-view");
                                                                       curso_content.classList.remove("curso-content-view"); }  });  }
 
-       const curso_img_watch = new IntersectionObserver(curso_img_view);
-       curso_img_watch.observe(curso_img); }
+                        const curso_img_watch = new IntersectionObserver(curso_img_view);
+                        curso_img_watch.observe(curso_img); }
+
+// Validaciones para seccion de curso container
+// Si ingresa por el telefono - inferior a 720px
+if(mq.matches) { inf_720px(); }
+
+// Si ingresa por el computador - superior a 720px 
+else { sup_720px(); }
+
