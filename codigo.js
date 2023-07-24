b1fb73874c63f004d67f58d12fe0c9989598784d
hablarEnPublico <shampoosnaturalesmasterclass@gmail.com>
2023-07-23 23:09:33 -0400

    Mejoras en el copy de la pagina y nueva seccion añadida(beneficios 2)

--- codigo.js
+++ codigo.js
@@ -3,7 +3,7 @@
 
 // Codigo seccion Slider o prueba social
 let comentarios = [`Lo estudie, lo puse en practica y ahora soy mejor comunicador. Tener un mentor como Carlos es y será siempre de bendición para mi vida, solo me queda seguir mejorando.`,`Estoy segura de que tendré un desarrollo increible con este entrenamiento. Conforme voy avanzando con las lecciones, voy entendiendo que hablar en público es un arte en el que puedes entrenarte.`,`Simplemente increible, muy buena introducción al programa, estoy seguro de que voy a sacarle mucho provecho y arrancar con todo en mis presentaciones de la universidad.`,`Soy licenciado y docente en la carrera de Contaduria, todo el tiempo debo comunicar frente a mis alumnos y por ello quiero hacerlo de la mejor manera. Sin duda alguna, es una habilidad que planeo inculcarles tambien, porque es muy importante.`,`Todo excelente, el contenido bien estructurado, facil de consumir.`];
-let nombres = ["Christian O. hace 4 semanas","Liliana P. hace 5 días","Miguel H. hace 2 semanas","Angel H. hace 2 semanas","Laura M. hace 1 semanas"]
+let nombres = ["Christian O. hace 4 semanas","Liliana P. hace 5 días","Miguel H. hace 2 semanas","Angel H. hace 2 semanas","Laura M. hace 1 semana"]
 
 let prueba_left = document.querySelector(".prueba-left");
 let prueba_derecha = document.querySelector(".prueba-derecha");
