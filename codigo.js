20baf4caa1e4b1e17ac625ec2d377b0ee49a8d74
hablarEnPublico <shampoosnaturalesmasterclass@gmail.com>
2023-09-18 19:32:52 -0400

    Mejoras en el copy de la pagina y precio rebajado (link cambiado)

--- codigo.js
+++ codigo.js
@@ -4,6 +4,6 @@
 //  Codigo seccion boton de compra
 const precio_comprar = document.querySelector(".precio-comprar");
 
-precio_comprar.addEventListener("click", ()=>{ window.open("https://go.hotmart.com/K81069854S?ap=8c44");  });
+precio_comprar.addEventListener("click", ()=>{ window.open("https://go.hotmart.com/K81069854S?ap=cb71");  });
 
 
